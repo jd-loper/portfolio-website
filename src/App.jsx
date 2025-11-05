faa30d14f596353e15943d3c2b95ebb43cf92fd3
Joseph Loper <jd.loper@icloud.com>
2025-11-05 00:28:46 -0700

    Add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import SiteNavbar from './components/SiteNavbar';
 import ProjectCards from './components/ProjectCards';
+import Hero from './components/Hero';
 
 export default function App() {
   return (
     <>
     <SiteNavbar brand="Portfolio" />
+    <Hero />
     <ProjectCards />
     </>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+    return (
+        <div class="container my-5">
+            <div class="p-5 text-center bg-body-tertiary rounded-3">
+                <h1 class="text-body-emphasis display-4">Hero Section</h1>
+                <p class="col-lg-8 mx-auto fs-5 text-muted">This is the hero section of the website.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
