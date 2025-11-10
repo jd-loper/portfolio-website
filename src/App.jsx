bd7a5b0b88e4f3e2fd5f801df2ee635a744779d4
Joseph Loper <jd.loper@icloud.com>
2025-11-09 19:48:26 -0700

    Add About and Contact components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import SiteNavbar from './components/SiteNavbar';
 import ProjectCards from './components/ProjectCards';
 import Hero from './components/Hero';
+import About from './components/About';
+import Contact from './components/Contact';
 
 export default function App() {
   return (
     <>
     <SiteNavbar />
     <Hero />
+    <About />
     <ProjectCards />
+    <Contact />
     </>
   )
 }
