53b772a34d5a3cf8ff5ff11db529ea76869ebb85
jd-loper <jd.loper@icloud.com>
2025-11-15 05:41:50 -0700

    Refactor class attributes to className in Hero and SiteNavbar components for consistency with JSX syntax

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 function Hero() {
     return (
-        <div class="container my-5">
-            <div class="p-5 text-center bg-body-tertiary rounded-3">
-                <h1 class="text-body-emphasis display-4">Hero Section</h1>
-                <p class="col-lg-8 mx-auto fs-5 text-muted">This is the hero section of the website.</p>
+        <div className="container my-5">
+            <div className="p-5 text-center bg-body-tertiary rounded-3">
+                <h1 className="text-body-emphasis display-4">Hero Section</h1>
+                <p className="col-lg-8 mx-auto fs-5 text-muted">This is the hero section of the website.</p>
             </div>
         </div>
     );
--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -1,17 +1,17 @@
 function SiteNavbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-dark fixed-top" data-bs-theme="dark">
-      <div class="container-fluid">
-        <span class="navbar-brand">My Portfolio</span>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-dark fixed-top" data-bs-theme="dark" aria-label="Primary navigation">
+      <div className="container-fluid">
+        <span className="navbar-brand">My Portfolio</span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbar">
-          <div class="navbar-nav">
-            <a class="nav-link" href="#">Home</a>
-            <a class="nav-link" href="#">About</a>
-            <a class="nav-link" href="#">Projects</a>
-            <a class="nav-link" href="#">Contact</a>
+        <div className="collapse navbar-collapse justify-content-end" id="navbar">
+          <div className="navbar-nav">
+            <a className="nav-link" href="#">Home</a>
+            <a className="nav-link" href="#about">About</a>
+            <a className="nav-link" href="#projects">Projects</a>
+            <a className="nav-link" href="#contact">Contact</a>
           </div>
         </div>
       </div>
